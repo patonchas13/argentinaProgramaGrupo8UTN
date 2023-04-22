659a83ebf5fe4aaf710daf228ec3d91df5a9434e
patonchas13 <fedemendoza1393@outlook.es>
2023-04-22 16:19:19 -0300

    creo metodos para obtener usuarios y validaciones contacto

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 
 $(document).ready(function(){
     console.log("Iniciando scripts");
-    //Scripts validacion reclamo
+
+    //Cargado de personas
+    console.log("Cargando personas");
+
+    fetch('https://reqres.in/api/users?page=2')
+    .then(response => response.text())
+    .then(texto => {
+      let obj = JSON.parse(texto);
+      mostrarUsuarios(obj);
+      console.log("Finalizado el cargado de personas");
+    })
+
+
+    function mostrarUsuarios(objetoJSON) {
+        let tbody = $('#personalTable');
+        let largo=4;
+        if(objetoJSON.data.length < largo){
+            largo=objetoJSON.data.length;
+        }
+        for (let i = 0; i < largo; i++) {
+          let user = objetoJSON.data[i];
+          let userDiv=document.createElement('div');
+          userDiv.innerHTML += `
+            <div>
+              <img src="${user.avatar}"></img>
+              <h4>${user.first_name} ${user.last_name}</h4>
+              <h4>${user.email}</h4>
+            </div>
+          `;
+          tbody.append(userDiv);
+        }
+    }
+
+    //Scripts validacion concurso
     $("#concurso-form").submit((event)=>{
         event.preventDefault(); // Evitar que el formulario se envíe automáticamente
         if(validarFormularioConcurso()){
@@ -30,6 +63,40 @@ $(document).ready(function(){
         }
         return true;
     }
+
+
+    //Scripts validacion contacto
+    $("#contacto-form").submit((event)=>{
+        event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+        if(validarFormularioContacto()){
+            // Limpiar el formulario
+            $("#contacto-form").trigger('reset');
+            // Mostrar el mensaje de éxito
+          //  $("#mensaje-exito-concurso").css("display","block");    
+        }else{
+            alert("Revisar campos del formulario de contacto");
+        }
+        
+    })
+
+    function validarFormularioContacto(){
+        let elementNombre=$("#nombreContacto").val();
+        if(estaVacio(elementNombre) || tieneNumero(elementNombre)){
+            return false;
+        }
+        let elementApellido=$("#apellidoContacto").val();
+        if(estaVacio(elementApellido) || tieneNumero(elementApellido)){
+            return false;
+        }
+        let elementEmail=$("#emailContacto").val();
+        if(!esCorreoElectronico(elementEmail)){
+            return false;
+        }
+        return true;
+    }
+
+
+
 
 
     //Scripts validacion reclamo
